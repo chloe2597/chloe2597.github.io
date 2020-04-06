4e84f2e0235555413ccac387762630bd656ac98d
chloe2597 <50755451+chloe2597@users.noreply.github.com>
2020-04-06 16:16:48 -0700

    Add files via upload

---
+++ Spices Project 2-Final/Cart.js
@@ -0,0 +1,108 @@
+﻿//by Collin Savunen and Chloe White
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btnremove')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    addItemToCart()
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addItemToCart() {
+    for (var i = 0; i < localStorage.length; i++) {
+        
+        var newitem = localStorage.getItem(localStorage.key(i))
+        if (newitem.length != 1) {
+            var item = {}
+            item = JSON.parse(newitem)
+            var title = item.title
+            var price = parseInt(item.price)
+            var imageSrc = item.imageSrc
+
+            var cartRow = document.createElement('div')
+            cartRow.classList.add('cart-row')
+            var cartRowContents = `
+                <div class="cart-item cart-column">
+                    <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+                    <p class="cart-item-title">${title}</p>
+                </div>
+                <div class="cart-quantity cart-column">
+                    <input class="cart-quantity-input" type="number" value="1">
+                    <button class="btn btnremove" type="button">REMOVE</button>
+                </div>
+                <span class="cart-price cart-column">${price}</span>
+                `
+            cartRow.innerHTML = cartRowContents
+            cartItems = document.getElementsByClassName('cart-items')[0]
+            cartItems.append(cartRow)
+            cartRow.getElementsByClassName('btnremove')[0].addEventListener('click', removeCartItem)
+            cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+        }
+        
+    }
+    updateCartTotal()
+
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    var counter = 1
+    localStorage.clear()
+    localStorage.setItem('counter', counter)
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+        var item = {}
+        item.title = cartRow.getElementsByClassName('cart-item-title')[0].innerText
+        item.price = price
+        item.imageSrc = cartRow.getElementsByClassName('cart-item-image')[0].innerText
+        localStorage.setItem('item' + counter, JSON.stringify(item))
+        counter++
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
---
+++ Spices Project 2-Final/add.js
@@ -0,0 +1,45 @@
+﻿if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var addToCartButtons = document.getElementsByClassName('btnAdd')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+    localStorage.setItem('counter', 1)
+   
+    function addToCartClicked(event) {
+        var button = event.target
+
+        var shopItem = button.parentElement.parentElement.parentElement
+        var itemTitle = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+       
+
+        for (var i = 0; i < localStorage.length; i++) {
+            var cartItem = localStorage.getItem(localStorage.key(i));
+            if (cartItem.length!=1) {
+                var item = {}
+                item = JSON.parse(cartItem)
+                cartTitle = item.title
+                if (cartTitle == itemTitle) {
+                    alert('This item is already added to the cart')
+                    return
+                }
+            }
+
+            
+        }
+        var item = {}
+        var counter = localStorage.getItem('counter')
+        item.title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+        item.price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+        item.imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+        localStorage.setItem('item' + counter, JSON.stringify(item))
+        counter++
+        localStorage.setItem('counter', counter)
+    }
+}
